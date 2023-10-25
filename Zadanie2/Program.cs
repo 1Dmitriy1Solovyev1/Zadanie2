57b2528350cbcb61e224291a0af6a6ae7bbcc172
user <user@ADMIN>
2023-10-25 10:27:16 +0300

    Добавьте файлы проекта.

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        if (double.TryParse(Console.ReadLine(), out double number))
+        {
+            if (number > 5 && number < 10)
+            {
+                Console.WriteLine("Число больше 5 и меньше 10.");
+            }
+            else
+            {
+                Console.WriteLine("Неизвестное число.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Ошибка при вводе числа.");
+        }
+    }
+}
